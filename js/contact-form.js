c115ae0ee552160e6583958785e5fab489a3a83b
Kristina Salnyk <kristina.salnyk@gmail.com>
2024-01-13 14:35:32 +0100

    feature(contact): added message sending

---
+++ js/contact-form.js
@@ -0,0 +1,26 @@
+(() => {
+  const refs = {
+    contactForm: document.querySelector('[data-contact-form]'),
+  };
+
+  const submitForm = event => {
+    event.preventDefault();
+
+    const formDate = new FormData(event.target);
+    const values = Object.fromEntries(formDate.entries());
+    if (!values.name || !values.email || !values.message) {
+      alert('Please fill in all the fields');
+      return;
+    }
+
+    emailjs
+      .sendForm('gmail', 'template', event.target)
+      .then(() => alert('Your message has been sent!'))
+      .catch(error => {
+        alert('Your message has not been sent!');
+        console.error(error);
+      });
+  };
+
+  refs.contactForm.addEventListener('submit', submitForm);
+})();
---
+++ js/email-service.js
@@ -0,0 +1,3 @@
+(function () {
+  emailjs.init('_egomZTDLbVZLgNMV');
+})();
